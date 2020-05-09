032cff12a42323f14b9a4c1635037178665add61
Maria LaRocca <mclarocca@gmail.com>
2020-05-08 20:50:26 -0400

    Style recipe cards-Desktop view, add lorem ipsum directions, add See recipes below message and animated arrow.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchButton = document.getElementById('searchbutton');
 const searchBar = document.getElementById('searchbar')
 
+
 // Trigger button on enter code from w3Schools:https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 
 // Execute a function when the user releases a key on the keyboard
@@ -16,8 +17,19 @@ searchBar.addEventListener("keyup", function (event) {
 
 
 searchButton.onclick = function (event) {
+  document.getElementById("see-below").style.visibility = "visible";
+  if (searchBar.value === "") {
+    return;
+  }
+
+  searchButton.style.color = '#759677';
+
+  let recipeResults = document.getElementById('recipelist')
+  recipeResults.innerHTML = '';
+
   const searchTerm = document.getElementById('searchbar').value;
   const url = `https://recipepuppyproxy.herokuapp.com/api/?i=${searchTerm}`;
+
 
   console.log("What is my url?", url)
 
@@ -30,31 +42,59 @@ searchButton.onclick = function (event) {
 
     recipeArray.forEach(function (recipe) {
       console.log('Recipe:', recipe.title)
-      console.log('Picture', recipe.href)
+      console.log('Picture', recipe.thumbnail)
       console.log('Ingredients', recipe.ingredients)
 
+      createRecipeCard(recipe);
     })
 
-    const recipeResults = document.getElementById('recipeCards')
+    document.getElementById('results-page').style.display = 'flex';
 
     // How to create a new element and define its design.
-    function createRecipeCard() {
+    function createRecipeCard(recipe) {
+      // const recipeResults = document.getElementById('recipelist')
       let newRecipeCard = document.createElement("div");
-      newRecipeCard.style.maxHeight = "500px";
-      newRecipeCard.style.minHeight = "300px";
-      newRecipeCard.style.maxWidth = "80%";
-      newRecipeCard.style.minWidth = "65%";
-      // newRecipeCard.style.display = "inline-block";
-      newRecipeCard.style.padding = "20px";
-      newRecipeCard.style.backgroundColor = "black";
-      recipeResults.appendChild(newRecipeCard)
+      newRecipeCard.classList.add('recipe-card')
+
+      console.log("What is recipe results?", recipeResults)
+      recipeResults.appendChild(newRecipeCard);
+
+      let recipeImage = document.createElement("img");
+      recipeImage.src = recipe.thumbnail;
+      recipeImage.classList.add('recipe-image')
+      // append img to div you just made
+      newRecipeCard.appendChild(recipeImage);
+
+      let recipeTitle = document.createElement("h2");
+      const recipeTitleWithoutLineBreaks = recipe.title.replace(/(\r\n|\n|\r)/gm, "");
+      recipeTitle.innerText = recipeTitleWithoutLineBreaks;
+      newRecipeCard.appendChild(recipeTitle);
+
+      let recipeIngredientsTitle = document.createElement("h3");
+      recipeIngredientsTitle.innerText = "Ingredients";
+      recipeIngredientsTitle.classList.add('ingredients-title')
+      newRecipeCard.appendChild(recipeIngredientsTitle);
+
+      let recipeIngredients = document.createElement("p");
+      recipeIngredients.innerText = recipe.ingredients;
+      newRecipeCard.appendChild(recipeIngredients);
+
+      let recipeDirectionsTitle = document.createElement("h3");
+      recipeDirectionsTitle.classList.add('directions-title')
+      recipeDirectionsTitle.innerText = "Directions"
+      newRecipeCard.appendChild(recipeDirectionsTitle);
+
+      let recipeDirections = document.createElement("p");
+      recipeDirections.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+      newRecipeCard.appendChild(recipeDirections)
+
     };
 
     // const allRecipeResults = document.querySelectorAll('#recipelist div');
 
     // console.log('Recipe Divs', allRecipeResults);
 
-    document.getElementById('results-page').style.display = 'flex';
+
 
   }
 
